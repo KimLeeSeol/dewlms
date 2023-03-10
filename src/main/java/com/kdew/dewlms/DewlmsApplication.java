a0eaf198bac781eecf18242bc7f11a1791e10208
kdew <kdew0308@gmail.com>
2023-03-10 19:31:57 +0900

    dewlms 초기화 설정

---
+++ src/main/java/com/kdew/dewlms/DewlmsApplication.java
@@ -0,0 +1,13 @@
+package com.kdew.dewlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DewlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DewlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdew/dewlms/DewlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdew.dewlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DewlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
